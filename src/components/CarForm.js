48a843a8b205d21bdb585eb4e68d3cc5a9dd17e9
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-05-07 11:02:56 +0100

    Update CarForm component

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,29 +1,54 @@
 import {useDispatch, useSelector} from 'react-redux'
-import {changeName} from '../store'
+import {addCar, changeCost, changeName} from '../store'
 
 const CarForm = () => {
-    const name = useSelector((state) => {
-        return state.form.name
+    const {cost, name} = useSelector((state) => {
+        return {
+            cost: state.form.cost,
+            name: state.form.name,
+        }
     })
     const dispatch = useDispatch()
-    const handleChangeName = (event) => {
+    const handleNameChange = (event) => {
         dispatch(changeName(event.target.value))
+    }
+    const handleCostChange = (event) => {
+        const carCost = parseInt(event.target.value) || 0
+        dispatch(changeCost(carCost))
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        dispatch(addCar({name, cost})) //from line 5
     }
     return (
         <div className="car-form panel">
             <h4 className="subtitle is-3">Add Car</h4>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="field-group">
                     <div className="field">
                         <label className="label">Name</label>
                         <input
                             className="input is-expanded"
                             value={name}
-                            onChange={handleChangeName}
+                            onChange={handleNameChange}
+                        />
+                    </div>
+
+                    {/* cost */}
+                    <div className="field">
+                        <label className="label">Cost</label>
+                        <input
+                            className="input is-expanded"
+                            value={cost || ''}
+                            onChange={handleCostChange}
+                            type="number"
                         />
                     </div>
                 </div>
             </form>
+            <div className="field">
+                <button className="button is-link">Submit</button>
+            </div>
         </div>
     )
 }
