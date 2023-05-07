69397dd7a248950bdc8b021a0ff10f41b3ad7fc7
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-05-07 10:32:08 +0100

    Create store file

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import {configureStore} from '@reduxjs/toolkit'
+import {
+    addCar,
+    carsReducer,
+    changeSearchTerm,
+    removeCar,
+} from './slices/carsSlice'
+import {changeCost, changeName, formReducer} from './slices/formSlice'
+
+const store = configureStore({
+    reducer: {
+        cars: carsReducer,
+        form: formReducer,
+    },
+})
+
+export {store, addCar, changeCost, changeName, changeSearchTerm, removeCar}
