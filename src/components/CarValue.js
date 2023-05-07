67db014e13ff3f4f9a2546b27624d3771b3eba9b
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-05-07 12:38:45 +0100

    Complete CarValue component

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,22 @@
+import {useSelector} from 'react-redux'
+
 const CarValue = () => {
-    return <div>CarValue</div>
+    const totalCost = useSelector(({cars: {data, searchTerm}}) => {
+        // const filteredCars = data.filter((car) => {
+        //     return car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        // })
+        // let cost = 0
+        // for (let car of filteredCars) {
+        //     cost += car.cost
+        // }
+        // return cost
+        return data
+            .filter((car) =>
+                car.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .reduce((acc, car) => (acc = car.cost), 0)
+    })
+    return <div className="car-value">Total Cost: ${totalCost}</div>
 }
 
 export default CarValue
