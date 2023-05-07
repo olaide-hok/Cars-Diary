228539d43c44edf3e902723e04f916198cf2d2d3
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-05-07 12:38:21 +0100

    Update CarList component.
    Applys bold class to car's name when entry contains similar character

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,10 +3,15 @@ import {removeCar} from '../store'
 
 const CarList = () => {
     const dispatch = useDispatch()
-    const cars = useSelector(({cars: {data, searchTerm}}) => {
-        return data.filter((car) =>
+    const {cars, name} = useSelector(({form, cars: {data, searchTerm}}) => {
+        const filteredCars = data.filter((car) =>
             car.name.toLowerCase().includes(searchTerm.toLowerCase())
         )
+
+        return {
+            cars: filteredCars,
+            name: form.name,
+        }
     })
 
     const handleCarDelete = (car) => {
@@ -14,8 +19,10 @@ const CarList = () => {
     }
 
     const renderedCars = cars.map((car) => {
+        const bold = name && car.name.toLowerCase().includes(name.toLowerCase())
+
         return (
-            <div key={car.id} className="panel">
+            <div key={car.id} className={`panel ${bold && 'bold'}`}>
                 <p>
                     {car.name} - ${car.cost}
                 </p>
